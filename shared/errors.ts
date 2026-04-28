7dbf56306e93d9d69db813c6c1a104b209fc7308
Richard Daniel <rdanieldesign@gmail.com>
2026-04-28 08:21:43 -0400

    refactor: remove unnecessary shared error handler

--- shared/errors.ts
+++ shared/errors.ts
@@ -2,11 +2,3 @@ export interface IError {
   status: number;
   message: string;
 }
-
-export function sendUnauthorizedMessage(): Promise<never> {
-  const error: IError = {
-    status: 403,
-    message: "You are not authorized to delete this record.",
-  };
-  return Promise.reject(error);
-}
